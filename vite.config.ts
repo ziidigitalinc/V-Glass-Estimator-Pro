196ec828fe82aa83377b993645a3adc2d6aca4ff
ziidigitalinc <ziidigitalinc@gmail.com>
2026-02-18 17:14:39 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig }  from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
+export default defineConfig({
+  // ... other config (plugins, build, etc.)
+  server: {
+    allowedHosts: true
+  }
+})
       },
       plugins: [react()],
       define: {
